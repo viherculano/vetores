e3330db094c430a0705929adf4a30147e2cc56db
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-17 22:45:14 -0300

    continuação ex07

--- src/ex07.java
+++ src/ex07.java
@@ -6,11 +6,25 @@ public class ex07 {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
         int[] x = new int[10];
+        int cont, grau = 0;
 
         for (int i = 0; i < x.length; i++) {
             x[i] = random.nextInt(1,10);
             System.out.print(x[i] + "\t");
 
         }
+
+        for (int i = 0; i < x.length; i++) {
+            cont = 0;
+            for (int j = 0; j < x.length; j++) {
+                if(x[i] == x[j]) {
+                    cont++;
+                }
+            }
+            if(cont > grau) {
+                grau = cont;
+            }
+        }
+        System.out.println(grau);
     }
 }
